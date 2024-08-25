0527174dafca84389206b0231848c562c1cec9f3
neder <neder_pereira@hotmail.com>
2024-08-25 14:46:23 -0300

    ✨ feat: add nova função para verificar usuario logado

---
+++ src/hooks/useAuthStatus.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+
+export const useAuthStatus = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsAuthenticated(true);
+        setUser(user);
+      } else {
+        setIsAuthenticated(false);
+        setUser(null);
+      }
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return { isAuthenticated, user, loading };
+};
