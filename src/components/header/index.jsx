5314245422789ad05e3d4ead7c867850e524ed33
neder <neder_pereira@hotmail.com>
2024-08-25 14:49:56 -0300

    🎨 feat: add validações e melhoria na estrutura

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -6,26 +6,28 @@ import {
   Flex,
   IconButton,
   Image,
-  useDisclosure,
 } from "@chakra-ui/react";
-import { useState } from "react";
 import { IoMdAdd } from "react-icons/io";
 import { cores } from "../../styles/cores";
 import { FormLogin } from "../FormLogin";
 import { useIsMobile } from "../../hooks/useIsMobile";
+import { useAuthStatus } from "../../hooks/useAuthStatus";
+import { LogOut } from "../LogOut";
 
 export const Header = () => {
-
+  const { isAuthenticated, user } = useAuthStatus();
   const isMobile = useIsMobile();
-  
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const [usuarioAuth, setUsuarioAuth] = useState(false);
 
   return (
-    <Flex alignItems="center" justifyContent="space-between" py={2} px={10}>
+    <Flex
+      alignItems="center"
+      justifyContent="space-between"
+      py={2}
+      px={isMobile ? 4 : 10}
+    >
       <Box>
         <Image
-          boxSize="70px"
+          boxSize={isMobile ? "50px" : "70px"}
           src={logo}
           objectFit="contain"
           alt="logo comuniconecta"
@@ -33,26 +35,33 @@ export const Header = () => {
       </Box>
 
       <Flex alignItems="center" gap={2}>
-        <FormLogin />
-        {!usuarioAuth ? (
-          <ButtonGroup size="sm" isAttached variant="outline">
+        {isAuthenticated ? (
+          <ButtonGroup mr={2} size="sm" isAttached variant="outline">
             {!isMobile && (
-              <Button sx={{
-                _hover: {
-                  bg: cores.verde,
-                }
-              }} color={cores.white}>Adicionar Evento</Button>
+              <Button
+                sx={{
+                  _hover: {
+                    bg: cores.verde,
+                  },
+                }}
+                color={cores.white}
+              >
+                Adicionar Evento
+              </Button>
             )}
             <IconButton
               aria-label="adicionar evento"
               sx={{
                 _hover: {
                   bg: cores.verde,
-                }}}
+                },
+              }}
               icon={<IoMdAdd color={cores.white} />}
             />
           </ButtonGroup>
         ) : null}
+        <FormLogin />
+        {user && <LogOut />}
       </Flex>
     </Flex>
   );
