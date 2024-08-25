ae1a211e23d0bec89fa5342436c6958980a5cfd1
neder <neder_pereira@hotmail.com>
2024-08-25 14:52:17 -0300

    🎨 wip: melhoria na estrutura

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { Header } from "./components/header";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { cores } from "./styles/cores";
-import { Apresentacao } from "./components/Apresentacao";
+import Home from "./pages/Home";
+import { SnackbarProvider } from "notistack";
 
 const theme = extendTheme({
   config: {
     initialColorMode: "light",
-    useSystemColorMode: false
+    useSystemColorMode: false,
   },
   styles: {
     global: {
@@ -20,8 +20,9 @@ const theme = extendTheme({
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Header />
-      <Apresentacao />
+      <SnackbarProvider maxSnack={3}>
+        <Home />
+      </SnackbarProvider>
     </ChakraProvider>
   );
 }
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import { Presentation } from "../../components/presentation";
+import { Header } from "../../components/header";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Presentation />
+    </>
+  );
+};
+
+export default Home;
