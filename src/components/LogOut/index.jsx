16c4e09bd29503500a761b396364e96522a81de9
neder <neder_pereira@hotmail.com>
2024-08-25 14:48:21 -0300

    ✨ feat: add compomente logout

---
+++ src/components/LogOut/index.jsx
@@ -0,0 +1,66 @@
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogOverlay,
+  Button,
+  IconButton,
+  useDisclosure,
+} from "@chakra-ui/react";
+import React from "react";
+import { RiLogoutCircleRLine } from "react-icons/ri";
+import { cores } from "../../styles/cores";
+import { getAuth, signOut } from "firebase/auth";
+import { useCustomSnackbar } from "../../hooks/useNotification";
+
+export const LogOut = () => {
+  const notificacao = useCustomSnackbar();
+  const cancelRef = React.useRef();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const handleLogout = async () => {
+    const auth = getAuth();
+    try {
+      await signOut(auth);
+      notificacao("Usuário deslogado com sucesso.", "success");
+    } catch (error) {
+      notificacao("Erro ao deslogar,", "error");
+    } finally {
+      onClose();
+    }
+  };
+
+  return (
+    <>
+      <IconButton
+        size="sm"
+        variant="ghost"
+        icon={<RiLogoutCircleRLine color={cores.white} size={22} />}
+        onClick={onOpen}
+      />
+      <AlertDialog
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+        isCentered
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent mx={2} bg={cores.verde}>
+            <AlertDialogBody mt={2} fontWeight="bold" color={cores.white}>
+              Você tem certeza que deseja sair?
+            </AlertDialogBody>
+            <AlertDialogFooter>
+              <Button color={cores.amarelo} ref={cancelRef} onClick={onClose}>
+                Cancelar
+              </Button>
+              <Button bg={cores.amarelo} onClick={handleLogout} ml={3}>
+                Sair
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+    </>
+  );
+};
