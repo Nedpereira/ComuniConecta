972eb66554d83c24cf653b5542e67190d4de9cd8
neder <neder_pereira@hotmail.com>
2024-08-25 14:41:38 -0300

    💄 style: add nova cor

--- src/styles/cores.js
+++ src/styles/cores.js
@@ -1,11 +1,12 @@
 export const cores = {
-  background: '#184848', // Verde Escuro
-  placeholder: '#457373', // Verde Cinza
-  verde: '#1A5959', // Verde Petróleo
-  verdeClaro: '#3A9F9F', // Verde Água
-  amarelo: '#A8C030', // Verde Amarelo
-  amareloClaro: '#EDCF32', // Amarelo Dourado
-  white: '#F9F7DA', // Marfim
-  ciano: '#17E7DB', // Ciano Brilhante
-  dourado: '#F5D547' // Amarelo Dourado
-}
+  background: "#184848", // Verde Escuro
+  placeholder: "#457373", // Verde Cinza
+  verde: "#1A5959", // Verde Petróleo
+  verdeClaro: "#3A9F9F", // Verde Água
+  amarelo: "#A8C030", // Verde Amarelo
+  amareloClaro: "#EDCF32", // Amarelo Dourado
+  white: "#F9F7DA", // Marfim
+  ciano: "#17E7DB", // Ciano Brilhante
+  dourado: "#F5D547", // Amarelo Dourado
+  cinza: "#7d7d7d",
+};
