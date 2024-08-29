5ec824339cba26f32fd17e13ce94b6fc650c7d62
neder <neder_pereira@hotmail.com>
2024-08-29 19:53:51 -0300

    🎨 feat: melhora na estrutura do código e novo params onEventCreated

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,20 +1,12 @@
 import logo from "../../assets/logo.png";
-import {
-  Box,
-  Button,
-  ButtonGroup,
-  Flex,
-  IconButton,
-  Image,
-} from "@chakra-ui/react";
-import { IoMdAdd } from "react-icons/io";
-import { cores } from "../../styles/cores";
+import { Box, Flex, Image } from "@chakra-ui/react";
 import { FormLogin } from "../FormLogin";
 import { useIsMobile } from "../../hooks/useIsMobile";
 import { useAuthStatus } from "../../hooks/useAuthStatus";
 import { LogOut } from "../LogOut";
+import { CreateEvent } from "../CreateEvent";
 
-export const Header = () => {
+export const Header = ({ onEventCreated }) => {
   const { isAuthenticated, user } = useAuthStatus();
   const isMobile = useIsMobile();
 
@@ -31,35 +23,12 @@ export const Header = () => {
           src={logo}
           objectFit="contain"
           alt="logo comuniconecta"
+          loading="lazy"
         />
       </Box>
 
       <Flex alignItems="center" gap={2}>
-        {isAuthenticated ? (
-          <ButtonGroup mr={2} size="sm" isAttached variant="outline">
-            {!isMobile && (
-              <Button
-                sx={{
-                  _hover: {
-                    bg: cores.verde,
-                  },
-                }}
-                color={cores.white}
-              >
-                Adicionar Evento
-              </Button>
-            )}
-            <IconButton
-              aria-label="adicionar evento"
-              sx={{
-                _hover: {
-                  bg: cores.verde,
-                },
-              }}
-              icon={<IoMdAdd color={cores.white} />}
-            />
-          </ButtonGroup>
-        ) : null}
+        {isAuthenticated && <CreateEvent onEventCreated={onEventCreated} />}
         <FormLogin />
         {user && <LogOut />}
       </Flex>
